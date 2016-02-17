9f15a2ed5100d3ec70651e80bed1956d36bf13cb
twarrre <tw4194@hotmail.com>
2016-02-17 14:59:43 -0800

    Added starter project with libGDX library.

---
+++ desktop/src/com/practicum/overtone/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.practicum.overtone.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.practicum.overtone.Overtone;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Overtone(), config);
+	}
+}
