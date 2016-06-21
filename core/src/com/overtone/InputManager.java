8c645f8fc792743dd0a11a88aca8fc8dde660d51
twarrre <tw4194@hotmail.com>
2016-06-21 15:29:25 -0700

    Implmented input manger to handle different types of input.

--- core/src/com/overtone/InputManager.java
+++ core/src/com/overtone/InputManager.java
@@ -1,68 +1,160 @@
 package com.overtone;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
+ * Handles input for the Game.
+ *
  * Created by trevor on 2016-05-01.
  */
 public class InputManager
 {
-
+    /**
+     * Represents all of the different types of inputs taken from the keyboard
+     */
     public enum ActionType
     {
         Pressed,
         Held,
         Released,
-        Down,
-        Up
+        Up;
+
+        // The size of the enum
+        private static final int size = KeyBinding.values().length;
     }
 
+    /**
+     * Stores all of the different key codes that are being tracked in the game
+     */
+    public enum KeyBinding
+    {
+        TopRight    (Input.Keys.I),
+        BottomRight (Input.Keys.K),
+        TopLeft     (Input.Keys.E),
+        BottomLeft  (Input.Keys.D);
+
+        KeyBinding(int key) { this.keyCode = key; }
+
+        // The code for the enum value
+        public int keyCode;
+
+        // The size of the enum
+        private static final int size = KeyBinding.values().length;
+    }
+
+    /**
+     * Stores the state of the keyboard from the previous frame
+     */
+    private Map<Integer, Boolean> _prevKeyboardState;
+
+    /**
+     *  Stores the current keyboard state for this frame.
+     */
+    private Map<Integer, Boolean> _currentKeyBoardState;
+
+    /**
+     * Constructor
+     */
     public InputManager()
     {
-
+        // Initialize the keyboard states to false
+        _currentKeyBoardState = new HashMap<Integer, Boolean>();
+        _prevKeyboardState = new HashMap<Integer, Boolean>();
+        KeyBinding[] bindings = KeyBinding.values();
+        for(int i = 0; i < KeyBinding.size; ++i)
+        {
+            _currentKeyBoardState.put(bindings[i].keyCode, false);
+            _prevKeyboardState.put(bindings[i].keyCode, false);
+        }
     }
 
-    public boolean ActionOccurred(String actionName, ActionType action)
+    /**
+     * Called every frame. Keeps track of the state of all keyboard keys being tracked.
+     */
+    public void Update()
+    {
+        for(Integer key : _currentKeyBoardState.keySet())
+        {
+            _prevKeyboardState.put(key, _currentKeyBoardState.get(key));
+            _currentKeyBoardState.put(key, Gdx.input.isKeyPressed(key));
+        }
+    }
+
+    /**
+     * Called to check if a particular keyboard action occurred and calls the appropriate method to handle the action.
+     * @param key - the key for which we are checking if an action occurred
+     * @param action - the action that we are checking to see if occurred
+     * @return true if the action occurred, false otherwise
+     */
+    public boolean ActionOccurred(KeyBinding key, ActionType action)
     {
         switch(action)
         {
             case Pressed:
-                return KeyPressed();
+                return KeyPressed(key.keyCode);
             case Held:
-                return KeyHeld();
+                return KeyHeld(key.keyCode);
             case Released:
-                return KeyReleased();
-            case Down:
-                return KeyDown();
+                return KeyReleased(key.keyCode);
             case Up:
-                return KeyUp();
+                return KeyUp(key.keyCode);
             default:
                 return false;
         }
     }
 
-    private boolean KeyPressed()
+    /**
+     * Checks to see if a key was pressed.
+     * @param key the key that we want to see if it was pressed
+     * @return true if the key was pressed, false otherwise
+     */
+    private boolean KeyPressed(int key)
     {
-        return false;
+        if(_prevKeyboardState.containsKey(key) && _currentKeyBoardState.containsKey(key))
+            return !_prevKeyboardState.get(key) && _currentKeyBoardState.get(key);
+        else
+            return false;
     }
 
-    private boolean KeyHeld()
+    /**
+     * Checks to see if a key was held.
+     * @param key the key that we want to see if it was held
+     * @return true if the key was held, false otherwise
+     */
+    private boolean KeyHeld(int key)
     {
-        return false;
+        if(_prevKeyboardState.containsKey(key) && _currentKeyBoardState.containsKey(key))
+            return _currentKeyBoardState.get(key) && _prevKeyboardState.get(key);
+        else
+            return false;
     }
 
-    private boolean KeyDown()
+    /**
+     * Checks to see if a key was released.
+     * @param key the key that we want to see if it was released
+     * @return true if the key was released, false otherwise
+     */
+    private boolean KeyReleased(int key)
     {
-        return false;
+        if(_prevKeyboardState.containsKey(key) && _currentKeyBoardState.containsKey(key))
+            return (!_currentKeyBoardState.get(key)) && _prevKeyboardState.get(key);
+        else
+            return false;
     }
 
-    private boolean KeyReleased()
+    /**
+     * Checks to see if a key is up.
+     * @param key the key that we want to see if it is up
+     * @return true if the key is up, false otherwise
+     */
+    private boolean KeyUp(int key)
     {
-        return false;
-    }
-
-    private boolean KeyUp()
-    {
-        return false;
+        if(_prevKeyboardState.containsKey(key) && _currentKeyBoardState.containsKey(key))
+            return !_currentKeyBoardState.get(key);
+        else
+            return false;
     }
 }
--- core/src/com/overtone/Overtone.java
+++ core/src/com/overtone/Overtone.java
@@ -10,21 +10,31 @@ import com.overtone.Screens.DifficultySelectScreen;
 import com.overtone.Screens.GameplayScreen;
 import com.overtone.Screens.MainMenuScreen;
 import com.overtone.Screens.SongCompleteScreen;
+import com.overtone.InputManager;
 
 public class Overtone extends ApplicationAdapter
 {
 	public Screen currentScreen;
+
 	private Array<Screen> _screens;
+	private InputManager _input;
 
 	@Override
 	public void create ()
 	{
+		_input = new InputManager();
+
 		_screens = new Array<Screen>();
 		_screens.add(new MainMenuScreen("background1.jpg", Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
 		_screens.add(new DifficultySelectScreen("background2.jpg", Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
 		_screens.add(new SongCompleteScreen("background3.jpg", Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
 		_screens.add(new GameplayScreen("background4.jpg", Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
 		currentScreen = _screens.get(0);
+
+		//currentScreen.hide();
+		//currentScreen = _screens.get(3);
+		//currentScreen.show();
+
 		currentScreen.show();
 	}
 
@@ -35,34 +45,27 @@ public class Overtone extends ApplicationAdapter
 		Gdx.gl.glClearColor(0, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		if(Gdx.input.isKeyPressed(Input.Keys.NUM_1))
+		_input.Update();
+
+		if(_input.ActionOccurred(InputManager.KeyBinding.BottomLeft, InputManager.ActionType.Pressed))
 		{
-			currentScreen.hide();
-			currentScreen = _screens.get(0);
-			currentScreen.show();
+			System.out.println("Key Pressed");
 		}
 
-		if(Gdx.input.isKeyPressed(Input.Keys.NUM_2))
+		if(_input.ActionOccurred(InputManager.KeyBinding.BottomLeft, InputManager.ActionType.Released))
 		{
-			currentScreen.hide();
-			currentScreen = _screens.get(1);
-			currentScreen.show();
+			System.out.println("Key Released");
 		}
 
-		if(Gdx.input.isKeyPressed(Input.Keys.NUM_3))
+		if(_input.ActionOccurred(InputManager.KeyBinding.BottomLeft, InputManager.ActionType.Held))
 		{
-			currentScreen.hide();
-			currentScreen = _screens.get(2);
-			currentScreen.show();
+			System.out.println("Key Held");
 		}
 
-		if(Gdx.input.isKeyPressed(Input.Keys.NUM_4))
+		if(_input.ActionOccurred(InputManager.KeyBinding.BottomLeft, InputManager.ActionType.Up))
 		{
-			currentScreen.hide();
-			currentScreen = _screens.get(3);
-			currentScreen.show();
+			System.out.println("Key Up");
 		}
-
 
 		currentScreen.render(deltaTime);
 	}
